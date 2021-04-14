30cb69cc1f418e15551005bf0850147343283c35
mgpm <massassipaul@gmail.com>
2021-04-15 00:01:36 +0200

    v5

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,4 +1,10 @@
 
 class dataAnalysis:
-    def __init__(self):
-        pass
+    def __init__(self,class_df):
+        self.class_df = class_df
+
+    def selectData(self,df_init):
+        self.df = df_init.select(self.class_df.values[0,:])
+
+
+
