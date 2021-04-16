00bc363a50b8395ad90efa4545ce13b0c782ed63
mgpm <massassipaul@gmail.com>
2021-04-16 16:32:19 +0200

    v6

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -3,8 +3,48 @@ class dataAnalysis:
     def __init__(self,class_df):
         self.class_df = class_df
 
-    def selectData(self,df_init):
-        self.df = df_init.select(self.class_df.values[0,:])
+    def selectData(self,df_init,pd):
+        colsSelect = [i for i in self.class_df.values[0,:]]+["label"]
+        self.df = df_init.select(colsSelect)
+        self.dfExp = self.df.toPandas()
+
+    def head(self):
+        self.dfExp.head()
+
+    def tail(self):
+        self.dfExp.tail()
+
+    def columns(self):
+        print(self.dfExp.columns)
+
+    def shape(self):
+        print(self.dfExp.shape)
+
+
+    def info(self):
+        self.dfExp.info()
+
+    def describe(self):
+        self.dfExp[self.dfExp.columns[:-1]].describe()
+
+    def HistoVarNum(self,plt):
+        self.dfExp(column=self.dfExp[:-1])
+        plt.show()
+
+    def BarVarCat(self,plt):
+        self.dfExp['label'].value_counts().plot(kind='bar',title='Target Variable')
+        plt.show()
+
+    def Correlation(self,sns,plt):
+        cor_mat = self.dfExp.corr()
+        sns.heatmap(cor_mat,annot=True)
+        plt.show()
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from pyspark.ml.feature import VectorAssembler as v,PCA,StandardScaler
 from pyspark.ml.clustering import KMeans as km
 from pyspark.sql import functions as func, types as t
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 
 from loadDataVector import loadDataVector
 from selectFeature import  selectFeature
-from clustering import clustering
+from clustering import clusters
 from dataAnalysis import dataAnalysis
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local[*]").appName("projetRCP216").getOrCreate()
